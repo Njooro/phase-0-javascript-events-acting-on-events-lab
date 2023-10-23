15ebb355ac49d18830d9ef5c7c708ad6fa321126
Maxwell <njoromax5@gmail.com>
2023-10-23 08:05:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "100px";
 dodger.style.bottom = "0px";
 dodger.style.left = "0px";
+
+
+
 document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
       const leftNumbers = dodger.style.left.replace("px", "");
@@ -13,8 +16,27 @@ document.addEventListener("keydown", function (event) {
       dodger.style.left = `${left - 1}px`;
     }
   });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left >= 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < window.innerWidth - dodger.clientWidth) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
       moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+      moveDodgerRight();
     }
   });
